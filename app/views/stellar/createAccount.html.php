f6beae2ad7507e0a09a5d195f0393320a9a44183
Nilam Doctor <nilamdoc@gmail.com>
2018-03-18 14:04:57 +0530

    accounts

--- app/views/stellar/createAccount.html.php
+++ app/views/stellar/createAccount.html.php
@@ -1,9 +1,12 @@
 <div class="container">
-<table class="table">
+<table class="table table-striped table-sm">
+<caption>List of Stellar Accounts</caption>
+<thead class="thead-light">
 <tr>
  <th>#</th>
- <th>Stellar Lumen Account</th>
+ <th>Stellar Lumen XLM Account</th>
 </tr> 
+</thead>
 <?php $i=1;foreach($accounts as $account){?>
 <tr>
  <td><?=$i?>
