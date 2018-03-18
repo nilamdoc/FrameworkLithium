20f28f3fe6f1c7f5b9c8491f04416a5bcdc820a9
Nilam Doctor <nilamdoc@gmail.com>
2018-03-18 13:59:44 +0530

    account

--- app/views/stellar/createAccount.html.php
+++ app/views/stellar/createAccount.html.php
@@ -1,6 +1,15 @@
-<?php
-foreach($accounts as $account){
- print_r($account['public']);
- print_r('<br>');
-}
-?>
+<div class="container">
+<table class="table">
+<tr>
+ <th>#</th>
+ <th>Stellar Lumen Account</th>
+</tr> 
+<?php $i=1;foreach($accounts as $account){?>
+<tr>
+ <td><?=$i?>
+<td>
+<code><?php  print_r($account['public']); ?></code>
+</td>
+</tr>
+<?php $i++;} ?>
+</div>
