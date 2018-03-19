e5013d92d6baee25c52afeac062df1969752f102
Nilam Doctor <nilamdoc@gmail.com>
2018-03-19 12:12:55 +0530

    transfer

--- app/controllers/StellarController.php
+++ app/controllers/StellarController.php
@@ -84,44 +84,49 @@ class StellarController extends \lithium\action\Controller {
   return compact('data');
  }
  public function transactions(){
-  if($this->request->data){
-  $server = Server::testNet();
-
-  // GAHC2HBHXSRNUT5S3BMKMUMTR3IIHVCARBFAX256NONXYKY65R2C5267
-  // You may need to fund this account if the testnet has been reset:
-  // https://www.stellar.org/laboratory/#account-creator?network=test
-  $sourceKeypair = Keypair::newFromSeed('SDU5L4Q7ZPW7AOKJTHZABI4QVD2YXPR4K2AFO7DD7HBYTO3PQPODGBER');
-  $destinationAccountId = 'GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5';
-
-  // Verify that the destination account exists. This will throw an exception
-  // if it does not
-  $destinationAccount = $server->getAccount($destinationAccountId);
-
-  // Build the payment transaction
-  $transaction = \ZuluCrypto\StellarSdk\Server::testNet()
-      ->buildTransaction($sourceKeypair->getPublicKey())
-      ->addOperation(
-          PaymentOp::newNativePayment($destinationAccountId, 1)
-      )
-  ;
-
-  // Sign and submit the transaction
-  $response = $transaction->submit($sourceKeypair->getSecret());
-
-  print "Response:" . PHP_EOL;
-  print_r($response->getRawData());
-
-  print PHP_EOL;
-  print 'Payment succeeded!' . PHP_EOL;
+  $accounts = Accounts::find('all');
+  $accountData = array();
+  foreach ($accounts as $account){
+   array_push($accountData, $this->getAccount($account['public']));
   }
- 
- $accounts = Accounts::find('all');
- $accountData = array();
- foreach ($accounts as $account){
-  array_push($accountData, $this->getAccount($account['public']));
+  return compact('accountData');
  }
- return compact('accountData');
- 
+
+ public function tranfer($pubkey){
+  if($this->request->data){
+   $server = Server::testNet();
+   $conditions = array('public'=>$pubkey);
+   $pair = Accounts::find('first', array(
+    'conditions'=>$conditions
+   ));
+   $secret = $pair['secret'];
+   // GAHC2HBHXSRNUT5S3BMKMUMTR3IIHVCARBFAX256NONXYKY65R2C5267
+   // You may need to fund this account if the testnet has been reset:
+   // https://www.stellar.org/laboratory/#account-creator?network=test
+   $sourceKeypair = Keypair::newFromSeed($secret);
+   $destinationAccountId = 'GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5';
+
+   // Verify that the destination account exists. This will throw an exception
+   // if it does not
+   $destinationAccount = $server->getAccount($destinationAccountId);
+
+   // Build the payment transaction
+   $transaction = \ZuluCrypto\StellarSdk\Server::testNet()
+       ->buildTransaction($sourceKeypair->getPublicKey())
+       ->addOperation(
+           PaymentOp::newNativePayment($destinationAccountId, 1)
+       )
+   ;
+
+   // Sign and submit the transaction
+   $response = $transaction->submit($sourceKeypair->getSecret());
+
+   print "Response:" . PHP_EOL;
+   print_r($response->getRawData());
+
+   print PHP_EOL;
+   print 'Payment succeeded!' . PHP_EOL;
+  }
  }
  
 }
